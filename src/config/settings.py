4ba6d95de93a314bdba5c7a47b2b4dbfcdbd79d8
Sergio LIVI <sergio@foodles.co>
2026-02-07 19:04:28 +0100

    Add minimal configuration to run in HA

--- src/config/settings.py
+++ src/config/settings.py
@@ -18,7 +18,11 @@ DEBUG = env.bool("DJANGO_DEBUG", default=False)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY", "django-insecure-vv-293486")
 
-ALLOWED_HOSTS = ["localhost", BASE_URL.hostname]
+ALLOWED_HOSTS = [
+    "localhost",
+    BASE_URL.hostname,
+    *env.list("ALLOWED_HOSTS", default=[]),
+]
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 CSRF_USE_SESSIONS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
