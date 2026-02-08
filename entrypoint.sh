2adcde2b1bef134f70eca72cb01c247c9d5f81cf
Sergio LIVI <sergio@foodles.co>
2026-02-08 16:21:09 +0100

    Read configuration in Home Assistant

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ if [[ $HOME_ASSISTANT_BUILD ]]; then
     echo "Configuring env variables for Home Assistant Supervisor"
     export DATABASE_URL=sqlite:////data/db.sqlite3
     export ALLOWED_HOSTS='*'
+
+    eval python -c "import json; options = json.loads(open('/data/options.json').read()); print('\n'.join(f'export {key.upper()}={value}' for key, value in options.items()))"
 fi
 
 django-admin migrate --noinput
