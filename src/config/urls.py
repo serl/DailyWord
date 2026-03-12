55c426ee4d976d6cd86d3532b284a13516abf955
Sergio Livi <me@serl.it>
2026-03-12 21:54:16 +0100

    Customize admin titles

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,6 +6,10 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
 
+admin.site.site_header = "DailyWord"
+admin.site.site_title = "DailyWord"
+admin.site.index_title = ""
+
 urlpatterns = [
     path("", lambda request: HttpResponse(), name="home"),
     path("admin/", admin.site.urls),
